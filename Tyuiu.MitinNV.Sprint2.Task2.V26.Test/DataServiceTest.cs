718de7e5376d4d9ac42b7d21a199d8008d050f5f
MitinNV <tyuiu.mitinnv@outlook.com>
2024-10-04 19:57:27 +0500

    Task2

---
+++ Tyuiu.MitinNV.Sprint2.Task2.V26.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.MitinNV.Sprint2.Task2.V26.Lib;
+
+namespace Tyuiu.MitinNV.Sprint2.Task2.V26.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            Assert.AreEqual(true, ds.CheckDotInShadedArea(8, 7));
+        }
+    }
+}
---
+++ Tyuiu.MitinNV.Sprint2.Task2.V26/Program.cs
@@ -0,0 +1,48 @@
+﻿using Tyuiu.MitinNV.Sprint2.Task2.V26.Lib;
+namespace Tyuiu.MitinNV.Sprint2.Task2.V26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #2 | Выполнил: Митин Никита Владимирович | ИИПБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Оператор if – полная и короткая форма записи                            *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #26                                                             *");
+            Console.WriteLine("* Выполнил: Митин Никита Владимирович | ИИПБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*Написать программу на, которая запрашивает целые значения с клавиатуры и *");
+            Console.WriteLine("* вычисляет находится ли точка с координатами X,Y в заштрихованной области*");
+            Console.WriteLine("*и.                                                                       *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            Console.WriteLine("Введите x:");
+            var x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите y:");
+            var y = Convert.ToInt32(Console.ReadLine());
+
+            var result = ds.CheckDotInShadedArea(x, y);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
