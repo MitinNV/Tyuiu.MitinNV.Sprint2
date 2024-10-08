cd502743ea8db6bbe9d8e3b63b59489f43bcd0aa
MitinNV <tyuiu.mitinnv@outlook.com>
2024-10-08 16:33:54 +0500

    Task6

---
+++ Tyuiu.MitinNV.Sprint2.Task6.V6.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.MitinNV.Sprint2.Task6.V6.Lib;
+
+namespace Tyuiu.MitinNV.Sprint2.Task6.V6.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            Assert.AreEqual("шестерка пик", ds.FindCardNameAndValue(1, 6));
+        }
+    }
+}
