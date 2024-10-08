6ad4889b299359e53f1816701cf2b8606fde6039
MitinNV <tyuiu.mitinnv@outlook.com>
2024-10-08 16:43:27 +0500

    Task7

---
+++ Tyuiu.MitinNV.Sprint2.Task7.V13.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.MitinNV.Sprint2.Task7.V13.Lib;
+
+namespace Tyuiu.MitinNV.Sprint2.Task7.V13.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService dataService = new DataService();
+            Assert.AreEqual(true, dataService.CheckDotInShadedArea(1, 1));
+        }
+    }
+}
---
+++ Tyuiu.MitinNV.Sprint2.Task7.V13/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.MitinNV.Sprint2.Task7.V13.Lib;
+namespace Tyuiu.MitinNV.Sprint2.Task7.V13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #2 | Выполнил: Митин Никита Владимирович | ИИПБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Добавление к решению итоговых проектов по спринту                       *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #13                                                             *");
+            Console.WriteLine("* Выполнил: Митин Никита Владимирович | ИИПБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*Написать программу на C#, которая запрашивает исходные данные (вещественн*");
+            Console.WriteLine("*ные значения) и вычисляет, находится ли точка с координатами X,Y в заштри*");
+            Console.WriteLine("*ихованной области.                                                       *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+            double x, y;
+            x = Convert.ToDouble(Console.ReadLine());
+            y = Convert.ToDouble(Console.ReadLine());
+            var result = ds.CheckDotInShadedArea(x, y);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
